03b1967d5e9c9882884a99eb06393d439a29b0f7
TheOdd <owenhines07@gmail.com>
2017-05-04 14:01:08 -0500

    Base package files

---
+++ spec/mapmycode-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import MapmycodeView from '../lib/mapmycode-view';
+
+describe('MapmycodeView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
